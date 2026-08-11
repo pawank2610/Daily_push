6ef3832d66d1f87b0f206104bded084701e692d3
Pawan Kumar <pawank2610@gmail.com>
2026-08-11 12:00:00 +0530

    Completed Week 2 Day 2 tasks

---
+++ week2/day1/day2/1.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req,res)=>{
+    if(req.url == "/"){
+        res.end("Home Page")
+    }else if(req.url== "/read" && req.method=="GET"){
+        //  const data = fs.readFileSync("./1.json", "utf-8")
+        // res.end(data)
+        const data = fs.createReadStream("./1.json","utf-8");
+        data.pipe(res);
+    
+    }else if(req.url=="/add"){
+        res.end("data created succesfull")
+    }else{
+        res.end("Not Found")
+    }
+})
+
+server.listen(8080,()=>{
+    console.log("server started")
+});
